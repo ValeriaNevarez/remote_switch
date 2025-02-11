530fc583dbe93a7f03cd302fc1e904fc15e61564
ValeriaNevarez <mvno.071095@hotmail.com>
2025-02-10 21:43:49 -0800

    Created login with authentication
    Created login authentication with firebase

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,19 @@
-import { useState } from "react";
-import "./App.css";
+import Layout from "./Layout";
+import Login from "./Login";
+import Profile from "./Profile";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  return <h1>Hello world</h1>;
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path = "/" element = { <Layout></Layout> }>
+            <Route index element = { <Login></Login> }></Route>
+            <Route path = "/profile" element = { <Profile></Profile> }></Route>
+          </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ app/src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCDgGTu9Je9qncnW_z8E0bsJhSYRDDkS8g",
+  authDomain: "remote-switch-6d907.firebaseapp.com",
+  projectId: "remote-switch-6d907",
+  storageBucket: "remote-switch-6d907.firebasestorage.app",
+  messagingSenderId: "368091458955",
+  appId: "1:368091458955:web:ded067debbc73b3afd138e",
+  measurementId: "G-ZCRHZ9R00F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth }
