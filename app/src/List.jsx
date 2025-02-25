402275e30491718fbf0899ee470b1ab6b2c5011d
ValeriaNevarez <mvno.071095@hotmail.com>
2025-02-24 19:50:48 -0800

    Remove usages of `firebase.jsx` directly.
    We should use `AuthProvider`.

--- app/src/List.jsx
+++ app/src/List.jsx
@@ -1,28 +1,36 @@
 import { useNavigate } from "react-router-dom";
-import auth, { database } from "./firebase";
-import { signOut } from "firebase/auth";
 import Header from "./Header";
-import { GetIdForSerialNumber, ReadDatabase, ChangeDeviceActive } from "./database_util";
+import {
+  GetIdForSerialNumber,
+  ReadDatabase,
+  ChangeDeviceActive,
+} from "./database_util";
 import React, { useState, useEffect } from "react";
-import {SendMessage, MakeCall, GetLastCallStatus, GetLastCompletedCallDate, GetStatusList} from "./twilio_util";
+import {
+  SendMessage,
+  MakeCall,
+  GetLastCallStatus,
+  GetLastCompletedCallDate,
+  GetStatusList,
+} from "./twilio_util";
 
+import { AuthContext } from "./AuthProvider";
+import { useContext } from "react";
 
 const List = () => {
   const navigate = useNavigate();
-  const user = auth.currentUser;
-  const [databaseContent, setDatabaseContents] = useState([]);
+  const { logOut } = useContext(AuthContext);
   const [id, setId] = useState(-1);
 
   const logoutUser = async (e) => {
     e.preventDefault();
-
-    await signOut(auth);
+    await logOut();
     navigate("/");
   };
 
-  // Codigo que se hace una vezcd 
+  // Codigo que se hace una vezcd
   useEffect(() => {
-    GetStatusList(['+16506697507', '+16508611877','pepe']);
+    GetStatusList(["+16506697507", "+16508611877", "pepe"]);
     // GetLastCompletedCallDate('+16508611877');
     // GetLastCallStatus('+16508611877').then((status)=> {console.log("react"+status)})
     // MakeCall('+16508611877');
@@ -37,7 +45,7 @@ const List = () => {
 
   // Termina codigo.
   useEffect(() => {
-    console.log(id)
+    console.log(id);
     // if (databaseContent.length != 0) {
     //   console.log(databaseContent);
     //   // console.log(databaseContent[0]['phone_number']);
