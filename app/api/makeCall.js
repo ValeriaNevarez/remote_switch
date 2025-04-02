71c50f5c02873644c5d87c19ad67140eac7d3831
ValeriaNevarez <mvno.071095@hotmail.com>
2025-04-01 19:47:10 -0700

    Added logic to makeCall function
    Calls now take pauses and play digits

--- app/api/makeCall.js
+++ app/api/makeCall.js
@@ -1,7 +1,6 @@
 const account_sid = process.env.TWILIO_ACCOUNT_SID;
 const auth_token = process.env.TWILIO_AUTH_TOKEN;
 
-
 import twilio from "twilio";
 
 export default async (req, res) => {
@@ -10,11 +9,37 @@ export default async (req, res) => {
 
   const client = twilio(account_sid, auth_token);
 
+  const VoiceResponse = twilio.twiml.VoiceResponse;
+
+  const response = new VoiceResponse();
+  response.pause({
+    length: 10,
+  });
+  // Prototype phone numbers that have inverted logic (wired backwards),
+  // digit 5 turns them off, digit 1 turns them on.
+  const is_inverted =
+    req.body.to == "+528713293364" ||
+    req.body.to == "+528713971819" ||
+    req.body.to == "+528713971823" ||
+    req.body.to == "+528713865040" ||
+    req.body.to == "+528713971807" ||
+    req.body.to == "+528713460690";
+
+  const digits = is_inverted == true ? "w1" : "w5";
+
+  response.play({
+    digits: digits,
+  });
+
+  response.pause({
+    length: 60,
+  });
+
   client.calls
     .create({
       from: "+18667487103",
       to: req.body.to,
-      url: "http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient",
+      twiml: response,
       timeLimit: 70,
     })
     .then((CallInstance) => {
